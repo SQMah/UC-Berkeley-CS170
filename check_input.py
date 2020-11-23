146f818bfdc8f31cb5d92a30d3df0630ec5fd385
Shao-Qian Mah <shaoqianmah@gmail.com>
2020-11-23 14:27:35 +0800

    Move in/out to samples dir

--- check_input.py
+++ check_input.py
@@ -1,12 +1,13 @@
 from input_generator import generate_input, generate_output
 from parse import read_output_file, validate_file, read_input_file
 from utils import calculate_stress_for_room, calculate_happiness_for_room, calculate_happiness, room_to_student
+import os
 
 if __name__ == "__main__":
     N = 50
     generate_input(*generate_output(N))
-    IN_FILE = f"{N}.in"
-    OUT_FILE = f"{N}.out"
+    IN_FILE = os.path.join("samples", f"{N}.in")
+    OUT_FILE = os.path.join("samples", f"{N}.out")
     if not validate_file(IN_FILE):
         raise ValueError("Invalid input file!")
     G, stress_budget = read_input_file(IN_FILE)
@@ -14,9 +15,9 @@ if __name__ == "__main__":
     D = read_output_file(OUT_FILE, G, stress_budget)
     print(f"TOTAL HAPPINESS: {calculate_happiness(D, G)}")
     print(f"STRESS BUDGET: {stress_budget}")
-    print(D)
+    print(f"Student to room: {D}")
     room_map = room_to_student(D)
-    print(room_map)
+    print(f"Room to students: {room_map}")
     for room in room_map:
         students = room_map[room]
         print(f"ROOM: {room}, students: {students}, happiness: {calculate_happiness_for_room(students, G)}, "
